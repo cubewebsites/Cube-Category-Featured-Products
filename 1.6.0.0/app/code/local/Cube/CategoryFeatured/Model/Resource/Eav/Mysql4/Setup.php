c3df49d60f68504b7f3536222ee2fca15f05a804
Cube Websites <ads@cubewebsites.com>
2012-02-04 11:37:11 +0000

    Featured attribute now created on module install

---
+++ 1.6.0.0/app/code/local/Cube/CategoryFeatured/Model/Resource/Eav/Mysql4/Setup.php
@@ -0,0 +1,40 @@
+<?php
+
+class Cube_CategoryFeatured_Model_Resource_Eav_Mysql4_Setup extends Mage_Eav_Model_Entity_Setup {
+	
+	public function getDefaultEntities() {
+		return array(
+			'catalog_product'	=>	array(
+				'entity_model'					=>	'catalog/product',
+				'attribute_model'				=>	'catalog/resource_eav_attribute',
+				'table'							=>	'catalog/product',
+				'additional_attribute_table'	=> 'catalog/eav_attribute',
+                'entity_attribute_collection'	=> 'catalog/product_attribute_collection',
+				'attributes'					=>	array(
+					'cube_category_featured'	=>	array(
+						'group'							=>	'Group/Tab',
+						'label'							=>	'Is Featured?',
+						'type'							=>	'boolean',
+						'input'							=>	'boolean',
+						'default'						=>	'0',
+						'class'							=>	'',
+						'backend'						=>	'',
+						'frontend'						=>	'',
+						'source'						=>	'',
+						'global'						=>	Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+						'visible'						=>	true,
+						'required'						=>	false,
+						'user_defined'					=>	true,
+						'searchable'					=>	false,
+						'filterable'					=>	false,
+						'comparable'					=>	false,
+						'visible_on_front'				=>	false,
+						'visible_in_advanced_search'	=>	false,
+						'unique'						=>	false
+					)
+				)
+			)
+		);
+	}
+	
+}
