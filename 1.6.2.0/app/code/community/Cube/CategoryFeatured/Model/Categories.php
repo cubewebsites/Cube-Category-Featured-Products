ce4f33fce57ff43a7510d365d11982052028f036
cubewebsites <ads@cubewebsites.com>
2012-02-04 17:39:54 +0000

    Confirmed working on Magento 1.6.2.0

---
+++ 1.6.2.0/app/code/community/Cube/CategoryFeatured/Model/Categories.php
@@ -0,0 +1,128 @@
+<?php
+
+/* Updated to make it easier to show which store a category belongs to (for multiple stores with same category names)
+ * Thanks to Eric
+ * http://www.cubewebsites.com/blog/magento/freebie-magento-featured-products-widget/comment-page-1/#comment-452
+ */
+
+class Cube_CategoryFeatured_Model_Categories {
+
+	public function toOptionArray() {
+		
+		//get a list of all active top level categories
+		$activeCategories = array();		
+        foreach ($this->getStoreCategories() as $child) {
+            if ($child->getIsActive()) {
+                $activeCategories[] = $child;
+            }
+        }
+        $activeCategoriesCount = count($activeCategories);
+        $hasActiveCategoriesCount = ($activeCategoriesCount > 0);		
+		
+		//no active categories - exit
+        if (!$hasActiveCategoriesCount) {
+            return array();
+        }
+		
+		//build up an array of active categories as value / label array
+		$ret = array();
+		foreach ($activeCategories as $category) {
+			$children = $this->getChildCategories($category);			
+			foreach($children as $k=>$v)
+				$ret[] = array(
+					'value' => $k,
+					'label' => $v
+				);
+		}
+		return $ret;
+		
+	}
+	
+	/**
+	 * Recursively returns a value / label array of all active categories
+	 * @param Mage_Catalog_Model_Category $category
+	 * @param String $parentname
+	 * @return array 
+	 */
+	private function getChildCategories($category,$parentname='') {
+		
+		//category not active - skip it
+		if (!$category->getIsActive()) {
+            return '';
+        }
+		
+		//array containing all the categories to return
+		$ret	=	array();
+		
+		/* Add the current category to return array
+		 * Root categories shouldn't be selected
+		 */
+		if($category->getLevel() > 1)
+			$ret[$category->getID()] = $category->getName() . $parentname;
+		
+		// get all children
+        if (Mage::helper('catalog/category_flat')->isEnabled()) {
+            $children = (array)$category->getChildrenNodes();
+            $childrenCount = count($children);
+        } else {
+            $children = $category->getChildrenCategories();
+            $childrenCount = $children->count();
+        }
+        $hasChildren = ($children && $childrenCount);
+		
+		// select active children
+        $activeChildren = array();
+        foreach ($children as $child) {
+            if ($child->getIsActive()) {
+                $activeChildren[] = $child;
+            }
+        }
+        $activeChildrenCount = count($activeChildren);
+        $hasActiveChildren = ($activeChildrenCount > 0);
+		
+		/**
+		 * Use recursion to include all children categories too
+		 */
+		foreach($activeChildren as $child) {			
+			$childarray = $this->getChildCategories($child, " / " . $category->getName() . $parentname);
+			foreach($childarray as $k => $v)
+				$ret[$k]	=	$v;
+		}		
+		
+		return $ret;
+	}
+	
+	/**
+	 * Gets the full path of parents for a given category
+	 * @param Mage_Catalog_Model_Category $category
+	 * @return string 
+	 */
+	private function __getParentName($category) {
+		
+		$parent_name = "";		
+		if($category->getParentID() && $category->getParentID() > 1) {			
+			$parent = Mage::getModel('catalog/category')->load($category->getParentID());
+			$parent_name .= " / {$parent->getName()}" . $this->__getParentName($parent);
+		}
+		
+		return  $parent_name;
+	}
+	
+	/**
+     * Get categories of current store
+     * @return Varien_Data_Tree_Node_Collection
+     */
+    public function getStoreCategories()
+    {
+        $collection = Mage::getModel('catalog/category')->getCollection()
+			->setStoreId($store)
+			->addAttributeToSelect('name')
+			->addAttributeToSelect('url_path')
+			->addAttributeToSelect('is_active')
+			->addAttributeToSort('parent_id', 'ASC')
+			->addFilter('parent_id',1)
+			->addAttributeToSort('name', 'ASC');
+		return $collection;
+    }
+
+}
---
+++ 1.6.2.0/app/code/community/Cube/CategoryFeatured/sql/categoryfeatured_setup/mysql4-install-0.0.1.php
@@ -0,0 +1,4 @@
+<?php
+
+$installer = $this;
+$installer->installEntities();
